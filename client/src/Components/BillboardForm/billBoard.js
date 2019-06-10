176da94aca95fe35aa33af3cd9b09b82a2ac8e5a
awaisdeveloper <getawaisdeveloper@gmail.com>
2019-06-10 10:17:48 +0500

    Biilboard from

--- client/src/Components/BillboardForm/billBoard.js
+++ client/src/Components/BillboardForm/billBoard.js
@@ -137,7 +137,7 @@ class BillBoard extends Component {
         e.preventDefault();
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
-                // console.log('Received values of form: ', values);
+                console.log('Received values of form: ', values);
                 // this.openNotification();
                 // this.setState({
                 //     sumitDataAlert: false
