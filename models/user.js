84adc4bf921fb4300701af2248b867216383c7cb
DevFarzan <farzanhanif123@gmail.com>
2019-07-01 02:59:43 -0700

    changing in all

--- models/user.js
+++ models/user.js
@@ -15,6 +15,7 @@ const userSchema = new Schema({
   landlineNo:{type:String},
   fullName:{type:String},
   type:{type:String},
+  role:{type:String},
   resetPasswordToken:{type:String},
   resetPasswordExpires:{type:String},
 });
