9fce7b3238affe881ab3dbcd999162d9bd06a035
devdaniyal <daniyalrazanoorani@gmail.com>
2019-08-28 10:24:57 -0700

    footer

--- client/src/Components/footer.js
+++ client/src/Components/footer.js
@@ -57,7 +57,7 @@ class Footer extends Component {
 							<Connect connect={connectObject} />
 					</div>
 					<div className="col-12 col-md-4 col-lg-4 col-xl-4">
-							<QuickLinks quicklinks={quicklinksObject}/>
+							<QuickLinks />
 					</div>
 
       				{/*<div className="col-4 col-md-4 col-lg-4 col-xl-4">
