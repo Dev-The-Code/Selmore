383022fb099da9bdbb3cf9ff18ba0b40ae613284
awaisdeveloper <getawaisdeveloper@gmail.com>
2019-10-12 11:00:59 +0500

    login drop down bug and length of array categories panel 1

--- client/src/Components/panel1.js
+++ client/src/Components/panel1.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './home.css';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { HttpUtils } from '../Services/HttpUtils';
 
 class Panel1 extends Component {
@@ -12,23 +12,14 @@ class Panel1 extends Component {
       categoryArr: ['Billboard ', 'Taxi Ads', 'Bus Ads', 'Bus Shelter Ads', 'Airport Ads', 'Shopping Mall',
         'Total Cinima Ads', 'Radio Ads', 'Other'],
       billboardcategory: [],
-      billboardcategoryCount: 0,
       taxiAdscategory: [],
-      taxiAdscategoryCount: 0,
       busAdscategory: [],
-      busAdscategorycount: 0,
       busShelterAdscategory: [],
-      busShelterAdscategoryCount: 0,
       airportAdscategory: [],
-      airportAdscategoryCount: 0,
       shoppingMallcategory: [],
-      shoppingMallcategoryCount: 0,
       cinimaAdscategory: [],
-      cinimaAdscategoryCount: 0,
       radioAdscategory: [],
-      radioAdscategoryCount: 0,
       othercategory: [],
-      othercategoryCount: 0,
       bilboardData: []
     }
   }
@@ -36,89 +27,61 @@ class Panel1 extends Component {
     let response = await HttpUtils.get('getbillboard');
     let data = response.content;
     let billboardcategoryArr = [];
-    let billboardcategoryNumber = 0;
     let taxiAdscategoryArr = [];
-    let taxiAdscategoryNumber = 0;
     let busAdscategoryArr = [];
-    let busAdscategoryNumber = 0;
     let busShelterAdscategoryArr = [];
-    let busShelterAdscategoryNumber = 0;
     let airportAdscategoryArr = [];
-    let airportAdscategoryNumber = 0;
     let shoppingMallcategoryArr = [];
-    let shoppingMallcategoryNumber = 0;
     let cinimaAdscategoryArr = [];
-    let cinimaAdscategoryNumber = 0;
     let radioAdscategoryArr = [];
-    let radioAdscategoryNumber = 0;
     let othercategoryArr = [];
-    let othercategoryNumber = 0;
     for (var i in data) {
       if (data[i].category[0] == 'Billboard ') {
         billboardcategoryArr.push(data[i]);
-        billboardcategoryNumber = billboardcategoryNumber + 1;
       }
       else if (data[i].category[0] == 'Taxi Ads') {
         taxiAdscategoryArr.push(data[i]);
-        taxiAdscategoryNumber = taxiAdscategoryNumber + 1;
       }
       else if (data[i].category[0] == 'Bus Ads') {
         busAdscategoryArr.push(data[i]);
-        busAdscategoryNumber = busAdscategoryNumber + 1;
       }
       else if (data[i].category[0] == 'Bus Shelter Ads') {
         busShelterAdscategoryArr.push(data[i]);
-        busShelterAdscategoryNumber = busShelterAdscategoryNumber + 1;
       }
       else if (data[i].category[0] == 'Airport Ads') {
         airportAdscategoryArr.push(data[i]);
-        airportAdscategoryNumber = airportAdscategoryNumber + 1;
       }
       else if (data[i].category[0] == 'Shopping Mall') {
 
         shoppingMallcategoryArr.push(data[i]);
-        shoppingMallcategoryNumber = shoppingMallcategoryNumber + 1;
       }
       else if (data[i].category[0] == 'Total Cinima Ads') {
         cinimaAdscategoryArr.push(data[i]);
-        cinimaAdscategoryNumber = cinimaAdscategoryNumber + 1;
       }
       else if (data[i].category[0] == 'Radio Ads') {
         radioAdscategoryArr.push(data[i]);
-        radioAdscategoryNumber = radioAdscategoryNumber + 1;
       }
       else if (data[i].category[0] == 'Other') {
         othercategoryArr.push(data[i]);
-        othercategoryNumber = othercategoryNumber + 1;
       }
     }
 
     this.setState({
       billboardcategory: billboardcategoryArr,
-      billboardcategoryCount: billboardcategoryNumber,
       taxiAdscategory: taxiAdscategoryArr,
-      taxiAdscategoryCount: taxiAdscategoryNumber,
       busAdscategory: busAdscategoryArr,
-      busAdscategorycount: busAdscategoryNumber,
       busShelterAdscategory: busShelterAdscategoryArr,
-      busShelterAdscategoryCount: busShelterAdscategoryNumber,
       airportAdscategory: airportAdscategoryArr,
-      airportAdscategoryCount: airportAdscategoryNumber,
       shoppingMallcategory: shoppingMallcategoryArr,
-      shoppingMallcategoryCount: shoppingMallcategoryNumber,
       cinimaAdscategory: cinimaAdscategoryArr,
-      cinimaAdscategoryCount: cinimaAdscategoryNumber,
       radioAdscategory: radioAdscategoryArr,
-      radioAdscategoryCount: radioAdscategoryNumber,
       othercategory: othercategoryArr,
-      othercategoryCount: othercategoryNumber,
     })
   }
   hoverAlert = (value) => {
     console.log(value)
     this.setState({
       directMarket: true,
-      keyValuee: value,
       bilboardData: value
     })
   }
@@ -127,25 +90,15 @@ class Panel1 extends Component {
     // const { directMarket, keyValuee } = this.state;
     const {
       billboardcategory,
-      billboardcategoryCount,
       taxiAdscategory,
-      taxiAdscategoryCount,
       busAdscategory,
-      busAdscategorycount,
       busShelterAdscategory,
-      busShelterAdscategoryCount,
       airportAdscategory,
-      airportAdscategoryCount,
       shoppingMallcategory,
-      shoppingMallcategoryCount,
       cinimaAdscategory,
-      cinimaAdscategoryCount,
       radioAdscategory,
-      radioAdscategoryCount,
       othercategory,
-      othercategoryCount,
       directMarket,
-      keyValuee,
       bilboardData
     } = this.state;
 
@@ -185,7 +138,7 @@ class Panel1 extends Component {
                     <img src="../images/1.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane3" >{`Bus Ads (${busAdscategorycount})`}</h5>
+                    <h5 className="lane3" >{`Bus Ads (${busAdscategory.length})`}</h5>
                   </div>
                 </div>
               </div>
@@ -196,7 +149,7 @@ class Panel1 extends Component {
                     <img src="../images/2.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane3">{`Taxi Ads (${taxiAdscategoryCount})`}</h5>
+                    <h5 className="lane3">{`Taxi Ads (${taxiAdscategory.length})`}</h5>
                   </div>
                 </div>
               </div>
@@ -206,7 +159,7 @@ class Panel1 extends Component {
                     <img src="../images/3.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane3">{`Billboard (${billboardcategoryCount})`}</h5>
+                    <h5 className="lane3">{`Billboard (${billboardcategory.length})`}</h5>
                   </div>
                 </div>
               </div>
@@ -220,7 +173,7 @@ class Panel1 extends Component {
                     <img src="../images/4.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane2">Shopping <br />{`Mall (${shoppingMallcategoryCount})`}</h5>
+                    <h5 className="lane2">Shopping <br />{`Mall (${shoppingMallcategory.length})`}</h5>
                   </div>
                 </div>
               </div>
@@ -231,7 +184,7 @@ class Panel1 extends Component {
                     <img src="../images/5.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane3">{`Airport Ads (${airportAdscategoryCount})`}</h5>
+                    <h5 className="lane3">{`Airport Ads (${airportAdscategory.length})`}</h5>
                   </div>
                 </div>
               </div>
@@ -241,7 +194,7 @@ class Panel1 extends Component {
                     <img src="../images/6.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane2">Bus Shelter <br />{`Ads (${busShelterAdscategoryCount})`}</h5>
+                    <h5 className="lane2">Bus Shelter <br />{`Ads (${busShelterAdscategory.length})`}</h5>
                   </div>
                 </div>
               </div>
@@ -255,7 +208,7 @@ class Panel1 extends Component {
                     <img src="../images/7.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane3">{`Other (${othercategoryCount})`}</h5>
+                    <h5 className="lane3">{`Other (${othercategory.length})`}</h5>
                   </div>
                 </div>
               </div>
@@ -265,7 +218,7 @@ class Panel1 extends Component {
                     <img src="../images/8.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane3">{`Radio Ads (${radioAdscategoryCount})`}</h5>
+                    <h5 className="lane3">{`Radio Ads (${radioAdscategory.length})`}</h5>
                   </div>
                 </div>
               </div>
@@ -275,7 +228,7 @@ class Panel1 extends Component {
                     <img src="../images/9.png" alt='img' className="lane1" />
                   </div>
                   <div className="col-md-9 col-9 col-sm-9">
-                    <h5 className="lane2">Total Cinema<br />{`Ads (${cinimaAdscategoryCount})`}</h5>
+                    <h5 className="lane2">Total Cinema<br />{`Ads (${cinimaAdscategory.length})`}</h5>
                   </div>
                 </div>
               </div>
