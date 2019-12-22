03209c80c7ea99ad7f4bcb1c48bf1f54e52fbfe7
DevFarzan <farzanhanif123@gmail.com>
2019-12-22 23:19:09 +0500

    changing in api's

---
+++ models/marketPlaceAvailibleBillboard.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+//Define model
+const marketPlaceAvailaibleBillboard = new Schema({
+        
+
+
+});
+
+//model class
+const ModelClass = mongoose.model('marketPlaceAvailaibleBillboard', marketPlaceAvailaibleBillboard);
+
+//export model
+module.exports = ModelClass;
