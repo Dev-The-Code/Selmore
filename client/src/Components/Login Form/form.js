fc3e86da0bd6b4bccc7bf3124fe88b563809945c
devawais <getawaisdeveloper@gmail.com>
2019-12-23 09:57:54 +0500

    form work

--- client/src/Components/Login Form/form.js
+++ client/src/Components/Login Form/form.js
@@ -33,14 +33,50 @@ class FormLogin extends Component {
     this.setState({ createAcountform: true })
   }
 
+  validateNumber(rule, value, callback) {
+    if (isNaN(value)) {
+      callback('Please type Numbers');
+    } else {
+      callback()
+    }
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        store.dispatch(logUser(this.state));
-        this.setState({ isLoader: true }, () => {
-        })
-        this.fectSignInApiFunc(values)
+        if (values.email != undefined && values.password != undefined) {
+          console.log("sing In hit")
+          // store.dispatch(logUser(this.state));
+          // this.setState({ isLoader: true }, () => {
+          // })
+          // this.fectSignInApiFunc(values)
+
+        }
+        else if (values.email != undefined && values.companyName != undefined && values.mobileNo != undefined && values.landlineNo != undefined) {
+          console.log('sing up hit')
+          // store.dispatch(logUser(this.state));
+          // this.setState({ isLoader: true }, () => {
+          // })
+          this.fectSignUpApiFunc(values)
+
+        }
+
+
+
+        // store.dispatch(logUser(this.state));
+        // this.setState({ isLoader: true }, () => {
+        // })
+        // this.fectSignInApiFunc(values)
+
+        // console.log(values, 'values')
+        // console.log(values.password, 'values')
+        // console.log(values.email, 'values')
+        // console.log(values.companyName, 'values')
+        // console.log(values.mobileNo, 'values')
+        // console.log(values.landlineNo, 'values')
+
+
       }
     });
   }
@@ -71,6 +107,13 @@ class FormLogin extends Component {
       }
     }
   }
+
+  fectSignUpApiFunc = async (values) => {
+    let response = await HttpUtils.post('signup', values);
+    console.log(response , 'response')
+
+  }
+
 
   render() {
     const { getFieldDecorator } = this.props.form;
@@ -145,8 +188,197 @@ class FormLogin extends Component {
                 </Form>
                 :
                 <div>
+                  <Form onSubmit={this.handleSubmit} className="login-form">
+
+                    <div className="row">
+                      <div className="col-12 col-md-6 col-lg-6 col-xl-6">
+                        <label style={{ marginBottom: '0px' }}>
+                          <span className="school10">
+                            Company name:
+                              </span>
+                        </label>
+                        <Form.Item>
+                          {getFieldDecorator('companyName', {
+                            rules: [{ required: true, message: 'Please enter your company name' }],
+                          })(
+                            <Input
+                              placeholder="Company name"
+                              className="bid_Input"
+                            />
+                          )}
+                        </Form.Item>
+                      </div>
+                      <div className="col-12 col-md-6 col-lg-6 col-xl-6">
+                        <label style={{ marginBottom: '0px' }}>
+                          <span className="school10">
+                            Email:
+                              </span>
+                        </label>
+                        <Form.Item>
+                          {getFieldDecorator('email', {
+                            rules: [{
+                              type: 'email',
+                              message: 'The input is not valid E-mail!',
+                            }, {
+                              required: true,
+                              message: 'Please input your E-mail!',
+                            }],
+                          })(
+                            <Input
+                              placeholder="Email"
+                              id={"exampleInputEmail1"}
+                              className="bid_Input"
+                            />
+                          )}
+                        </Form.Item>
+                      </div>
+
+                    </div><br />
+                    <div className="row" style={{ marginTop: '1vw' }}>
+                      <div className="col-12 col-md-6 col-lg-6 col-xl-6">
+                        <label style={{ marginBottom: '0px' }}>
+                          <span className="school10">
+                            Landline no :
+                              </span>
+                        </label>
+                        <Form.Item>
+                          {getFieldDecorator('landlineNo', {
+                            // initialValue: this.state.dataBnumber,
+                            rules: [{
+                              required: true,
+                              message: 'Please input your lanline number!',
+                              whitespace: true
+                            },
+                            { validator: this.validateNumber.bind(this) }]
+                          })(
+                            <Input
+                              placeholder="Landline no"
+                              className="bid_Input"
+                            />
+                          )}
+                        </Form.Item>
+                      </div>
+                      <div className="col-12 col-md-6 col-lg-6 col-xl-6">
+                        <label style={{ marginBottom: '0px' }}>
+                          <span className="school10">
+                            Mobile no :
+                              </span>
+                        </label>
+                        <Form.Item>
+                          {getFieldDecorator('mobileNo', {
+                            // initialValue: this.state.dataBnumber,
+                            rules: [{
+                              required: true,
+                              message: 'Please input your mobile Number!',
+                              whitespace: true
+                            },
+                            { validator: this.validateNumber.bind(this) }]
+                          })(
+                            <Input
+                              type="number"
+                              placeholder="Mobile no"
+                              className="bid_Input"
+                            />
+                          )}
+                        </Form.Item>
+                      </div>
+                    </div><br />
+                    <div className="row">
+                      <div className="col-12 col-md-12 col-lg-12 col-xl-12">
+                        <button type="submit" className="btn btn-primary"><span className="school5">Sign Up</span></button>
+                        <p className="" style={{ margin: '1vw 0px 0px' }}>
+                          <span className="alreadyAcontTextSignup">Already have an account ?</span><span className="SignInformText" onClick={this.alreadyHaveacount}>Sign In</span>
+                        </p>
+                      </div>
+                    </div>
+                  </Form>
+                </div>
+              }
+            </div>
+            {isLoader ? <div class="loading"> 	</div>
+              : null
+            }
+
+
+
+
+            <div className="col-md-1">
+            </div>
+          </div>
+        </div>
+
+
+
+
+        {/* mobile screen */}
+        <div className="d-block d-sm-none">
+          <div className="row school1">
+            <div className="col-md-4 school7">
+              <img src="../images/log-in.png" alt='img' style={{ width: '100%', height: '257px' }} />
+            </div>
+            <div className="col-md-4 school6">
+              {this.state.createAcountform ?
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                  <div className="form-group">
+                    <label for="exampleInputEmail1" style={{ marginBottom: '0px' }}>
+                      <span className="school3">
+                        Email address:
+                  </span>
+                    </label>
+                    <Form.Item>
+                      {getFieldDecorator('email', {
+                        rules: [{
+                          type: 'email',
+                          message: 'The input is not valid E-mail!',
+                        }, {
+                          required: true,
+                          message: 'Please input your E-mail!',
+                        }],
+                      })(
+                        <Input
+                          type="text"
+                          className={"form-control"}
+                          id={"exampleInputEmail1"}
+                          name="username"
+                          placeholder="Email:*"
+                        />
+                      )}
+                    </Form.Item>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1" style={{ marginBottom: '0px' }}>
+                      <span className="school3"
+                      >Password:
+                  </span>
+                    </label>
+                    <Form.Item>
+                      {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input your Password!' }],
+                      })(
+                        <Input type="password"
+                          className={"form-control"}
+                          id={"exampleInputPassword1"}
+                          placeholder="Password" />
+                      )}
+                    </Form.Item>
+                  </div>
+                  <p style={{ marginTop: '-4%' }}><span className="school8">Forget Password!?</span></p>
+                  <button type="submit" className="btn btn-primary"><span className="school5">Login</span></button>
+                  <p onClick={this.CreateUserForm} className="" style={{ margin: '1vw 0px 0px' }}>
+                    <span className="alreadyAcontText">Create an account ?</span>
+                  </p>
+                  <br />
+                  {isAlert ?
+                    <div class="alert alert-danger" role="alert">
+                      Please cheak your email or password
+            </div>
+                    : null
+                  }
+                </Form>
+                :
+                <div>
                   <div className="row">
-                    <div className="col-12 col-md-6 col-lg-6 col-xl-6">
+                    <div className="col-12">
                       <label style={{ marginBottom: '0px' }}>
                         <span className="school10">
                           Company name:
@@ -157,7 +389,7 @@ class FormLogin extends Component {
                         className="bid_Input"
                       />
                     </div>
-                    <div className="col-12 col-md-6 col-lg-6 col-xl-6">
+                    <div className="col-12">
                       <label style={{ marginBottom: '0px' }}>
                         <span className="school10">
                           Email:
@@ -169,8 +401,8 @@ class FormLogin extends Component {
                       />
                     </div>
                   </div><br />
-                  <div className="row" style={{ marginTop: '1vw' }}>
-                    <div className="col-12 col-md-6 col-lg-6 col-xl-6">
+                  <div className="row" style={{ marginTop: '-4vw' }}>
+                    <div className="col-12">
                       <label style={{ marginBottom: '0px' }}>
                         <span className="school10">
                           Landline no :
@@ -181,7 +413,7 @@ class FormLogin extends Component {
                         className="bid_Input"
                       />
                     </div>
-                    <div className="col-12 col-md-6 col-lg-6 col-xl-6">
+                    <div className="col-12">
                       <label style={{ marginBottom: '0px' }}>
                         <span className="school10">
                           Mobile no :
@@ -195,150 +427,15 @@ class FormLogin extends Component {
                     </div>
                   </div><br />
                   <div className="row">
-                    <div className="col-12 col-md-12 col-lg-12 col-xl-12">
+                    <div className="col-12">
                       <button type="submit" className="btn btn-primary"><span className="school5">Sign Up</span></button>
                       <p className="" style={{ margin: '1vw 0px 0px' }}>
-                        <span className="alreadyAcontTextSignup">Already have an account ?</span><span className="SignInformText" onClick={this.alreadyHaveacount}>Sign In</span>
+                        <span className="alreadyAcontText">Already have an account ?</span><span className="SignInformText" onClick={this.alreadyHaveacount}>Sign In</span>
                       </p>
                     </div>
                   </div>
                 </div>
               }
-            </div>
-            {isLoader ? <div class="loading"> 	</div>
-              : null
-            }
-
-
-            <div className="col-md-1">
-            </div>
-          </div>
-        </div>
-        <div className="d-block d-sm-none">
-          <div className="row school1">
-            <div className="col-md-4 school7">
-              <img src="../images/log-in.png" alt='img' style={{ width: '100%', height: '257px' }} />
-            </div>
-            <div className="col-md-4 school6">
-             {this.state.createAcountform ?
-              <Form onSubmit={this.handleSubmit} className="login-form">
-                <div className="form-group">
-                  <label for="exampleInputEmail1" style={{ marginBottom: '0px' }}>
-                    <span className="school3">
-                      Email address:
-                  </span>
-                  </label>
-                  <Form.Item>
-                    {getFieldDecorator('email', {
-                      rules: [{
-                        type: 'email',
-                        message: 'The input is not valid E-mail!',
-                      }, {
-                        required: true,
-                        message: 'Please input your E-mail!',
-                      }],
-                    })(
-                      <Input
-                        type="text"
-                        className={"form-control"}
-                        id={"exampleInputEmail1"}
-                        name="username"
-                        placeholder="Email:*"
-                      />
-                    )}
-                  </Form.Item>
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1" style={{ marginBottom: '0px' }}>
-                    <span className="school3"
-                    >Password:
-                  </span>
-                  </label>
-                  <Form.Item>
-                    {getFieldDecorator('password', {
-                      rules: [{ required: true, message: 'Please input your Password!' }],
-                    })(
-                      <Input type="password"
-                        className={"form-control"}
-                        id={"exampleInputPassword1"}
-                        placeholder="Password" />
-                    )}
-                  </Form.Item>
-                </div>
-                <p style={{ marginTop: '-4%' }}><span className="school8">Forget Password!?</span></p>
-                <button type="submit" className="btn btn-primary"><span className="school5">Login</span></button>
-                <p onClick={this.CreateUserForm} className="" style={{ margin: '1vw 0px 0px' }}>
-                  <span className="alreadyAcontText">Create an account ?</span>
-                </p>
-                <br />
-                {isAlert ?
-                  <div class="alert alert-danger" role="alert">
-                    Please cheak your email or password
-            </div>
-                  : null
-                }
-              </Form>
-             :
-              <div>
-                <div className="row">
-                  <div className="col-12">
-                    <label style={{ marginBottom: '0px' }}>
-                      <span className="school10">
-                        Company name:
-                              </span>
-                    </label>
-                    <Input
-                      placeholder="Company name"
-                      className="bid_Input"
-                    />
-                  </div>
-                  <div className="col-12">
-                    <label style={{ marginBottom: '0px' }}>
-                      <span className="school10">
-                        Email:
-                              </span>
-                    </label>
-                    <Input
-                      placeholder="Email"
-                      className="bid_Input"
-                    />
-                  </div>
-                </div><br />
-                <div className="row" style={{marginTop: '-4vw'}}>
-                  <div className="col-12">
-                    <label style={{ marginBottom: '0px' }}>
-                      <span className="school10">
-                        Landline no :
-                              </span>
-                    </label>
-                    <Input
-                      placeholder="Landline no"
-                      className="bid_Input"
-                    />
-                  </div>
-                  <div className="col-12">
-                    <label style={{ marginBottom: '0px' }}>
-                      <span className="school10">
-                        Mobile no :
-                              </span>
-                    </label>
-                    <Input
-                      type="number"
-                      placeholder="Mobile no"
-                      className="bid_Input"
-                    />
-                  </div>
-                </div><br />
-                <div className="row">
-                  <div className="col-12">
-                    <button type="submit" className="btn btn-primary"><span className="school5">Sign Up</span></button>
-                    <p className="" style={{ margin: '1vw 0px 0px' }}>
-                      <span className="alreadyAcontText">Already have an account ?</span><span className="SignInformText" onClick={this.alreadyHaveacount}>Sign In</span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-             } 
             </div>
             {isLoader ? <div class="loading1">   </div>
               : null
